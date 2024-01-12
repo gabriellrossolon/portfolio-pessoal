c5162722f4f50fdf461376aa8c5e544b9a45055b
Gabriell Rossolon <gabrirossolon@yahoo.com.br>
2024-01-12 16:48:15 -0400

    ajuste switchMode para total JS

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#switch')
 
-$checkbox.addEventListener('change', function() {
-    $html.classList.toggle('light-mode')
-})
+// $checkbox.addEventListener('change', function() {
+//     $html.classList.toggle('light-mode')
+// })
 
 //
 AOS.init();
+
+
+function changeMode() {
+    $html.classList.toggle('light-mode');
+}
+
+function changeIcon() {
+    if (switchButton.classList.contains("fa-moon")){
+        switchButton.classList.remove("fa-moon") || switchButton.classList.add("fa-sun")
+    } else {
+        switchButton.classList.remove("fa-sun") || switchButton.classList.add("fa-moon") 
+    } 
+}  
